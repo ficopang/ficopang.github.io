5765e432c520395707dc488c2efce35ff7feb4dd
ficopang <pangestufico@gmail.com>
2020-08-22 11:35:44 +0700

    Updates

---
+++ precache-manifest.bc24c17601e335f43a93721dc086fec9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a3ae9404b5e406570481e1bddeca895",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c84967e728720edaf8af",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "020a8c56d58bbb26c5f0",
+    "url": "/static/js/2.0d45e836.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0d45e836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c84967e728720edaf8af",
+    "url": "/static/js/main.2f40d2a8.chunk.js"
+  },
+  {
+    "revision": "ddc3ab74a8965e9f249d",
+    "url": "/static/js/runtime-main.36e53336.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
