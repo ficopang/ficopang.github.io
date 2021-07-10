17f80c7cc9015786f2f6d0c90943cb1c48486abf
ficopang <pangestufico@gmail.com>
2021-07-10 11:16:45 +0700

    Profile v3

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: {
+    enabled: false,
+    content: ["./*.html", "./*.js"],
+    safelist: [],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
